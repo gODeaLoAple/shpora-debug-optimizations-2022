902e90da8d487fdda3cd9a322e2c3e9b33c465e1
gODealOAple <maksim.math@gmail.com>
2022-03-21 20:59:18 +0500

    Refactor pixels

---
+++ optimizations/JPEG/Images/PixelRgb.cs
@@ -0,0 +1,12 @@
+﻿namespace JPEG.Images;
+
+public class PixelRgb
+{
+    public byte R { get; set; }
+    public byte G { get; set; }
+    public byte B { get; set; }
+        
+    public double Y => 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+    public double Cb => 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+    public double Cr => 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+}
