4ef0ff098b0e12774b7388137e914937c2b66fa5
gODealOAple <maksim.math@gmail.com>
2022-03-22 00:36:21 +0500

    Made encoding faster

---
+++ optimizations/JPEG/Encoding/HuffmanCodec.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JPEG;
+
+internal static class HuffmanCodec
+{
+    public static byte[] Encode(ICollection<byte> data, out Dictionary<BitsWithLength, byte> decodeTable, out long bitsCount)
+    {
+        var frequences = CalcFrequences(data);
+
+        var root = BuildHuffmanTree(frequences);
+
+        var encodeTable = new BitsWithLength[byte.MaxValue + 1];
+        FillEncodeTable(root, encodeTable);
+
+        var bitsBuffer = new BitsBuffer();
+        foreach(var b in data)
+            bitsBuffer.Add(encodeTable[b]);
+
+        decodeTable = CreateDecodeTable(encodeTable);
+
+        return bitsBuffer.ToArray(out bitsCount);
+    }
+
+    public static byte[] Decode(byte[] encodedData, Dictionary<BitsWithLength, byte> decodeTable, long bitsCount)
+    {
+        var result = new List<byte>();
+
+        byte decodedByte;
+        var sample = new BitsWithLength { Bits = 0, BitsCount = 0 };
+        for(var byteNum = 0; byteNum < encodedData.Length; byteNum++)
+        {
+            var b = encodedData[byteNum];
+            for(var bitNum = 0; bitNum < 8 && byteNum * 8 + bitNum < bitsCount; bitNum++)
+            {
+                sample.Bits = (sample.Bits << 1) + ((b & (1 << (8 - bitNum - 1))) != 0 ? 1 : 0);
+                sample.BitsCount++;
+
+                if(decodeTable.TryGetValue(sample, out decodedByte))
+                {
+                    result.Add(decodedByte);
+
+                    sample.BitsCount = 0;
+                    sample.Bits = 0;
+                }
+            }
+        }
+        return result.ToArray();
+    }
+
+    private static Dictionary<BitsWithLength, byte> CreateDecodeTable(BitsWithLength[] encodeTable)
+    {
+        var result = new Dictionary<BitsWithLength, byte>(new BitsWithLength.Comparer());
+        for(var b = 0; b < encodeTable.Length; b++)
+        {
+            var bitsWithLength = encodeTable[b];
+            if(bitsWithLength == null)
+                continue;
+
+            result[bitsWithLength] = (byte) b;
+        }
+        return result;
+    }
+
+    private static void FillEncodeTable(HuffmanNode node, BitsWithLength[] encodeSubstitutionTable, int bitvector = 0, int depth = 0)
+    {
+        while (true)
+        {
+            if (node.LeafLabel != null)
+            {
+                encodeSubstitutionTable[node.LeafLabel.Value] = new BitsWithLength { Bits = bitvector, BitsCount = depth };
+            }
+            else
+            {
+                if (node.Left != null)
+                {
+                    FillEncodeTable(node.Left, encodeSubstitutionTable, (bitvector << 1) + 1, depth + 1);
+                    node = node.Right;
+                    bitvector = (bitvector << 1) + 0;
+                    depth = depth + 1;
+                    continue;
+                }
+            }
+
+            break;
+        }
+    }
+
+    private static HuffmanNode BuildHuffmanTree(int[] frequences)
+    {
+        var huffmanNodes = GetNodes(frequences);
+        var nodes = huffmanNodes.ToHashSet();
+        while(nodes.Count > 1)
+        {
+            HuffmanNode firstMin = null;
+            HuffmanNode secondMin = null;
+
+            foreach (var node in nodes)
+            {
+                if (firstMin is null)
+                {
+                    firstMin = node;
+                }
+                else if (secondMin is null)
+                {
+                    secondMin = node;
+                }
+                else
+                {
+                    if (node.Frequency < firstMin.Frequency)
+                    {
+                        secondMin = firstMin;
+                        firstMin = node;
+                    }
+                    else if (node.Frequency < secondMin.Frequency)
+                    {
+                        secondMin = node;
+                    }
+                }
+            }
+
+            nodes.Remove(firstMin);
+            nodes.Remove(secondMin);
+            
+            var root = new HuffmanNode(secondMin, firstMin);
+            nodes.Add(root);
+        }
+
+        return nodes.First();
+    }
+
+    private static IEnumerable<HuffmanNode> GetNodes(int[] frequences)
+    {
+        for (var i = 0; i <= byte.MaxValue; i++)
+        {
+            if (frequences[i] > 0)
+            {
+                yield return new HuffmanNode((byte)i, frequences[i]);
+            }
+        }
+    }
+
+    private static int[] CalcFrequences(IEnumerable<byte> data)
+    {
+        var result = new int[byte.MaxValue + 1];
+        Parallel.ForEach(data, b => Interlocked.Increment(ref result[b]));
+        return result;
+    }
+}
---
+++ optimizations/JPEG/Encoding/HuffmanNode.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JPEG;
+
+internal class HuffmanNode : IEquatable<HuffmanNode>, IComparable<HuffmanNode>, IComparable
+{
+    public int CompareTo(HuffmanNode other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        var frequencyComparison = Frequency.CompareTo(other.Frequency);
+        if (frequencyComparison != 0) return frequencyComparison;
+        return _id.CompareTo(other._id);
+    }
+
+    public int CompareTo(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return 1;
+        if (ReferenceEquals(this, obj)) return 0;
+        return obj is HuffmanNode other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(HuffmanNode)}");
+    }
+
+    public bool Equals(HuffmanNode other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _id == other._id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((HuffmanNode)obj);
+    }
+
+    public static bool operator ==(HuffmanNode left, HuffmanNode right) => Equals(left, right);
+
+    public static bool operator !=(HuffmanNode left, HuffmanNode right) => !Equals(left, right);
+
+    private static int Id;
+
+
+    public readonly byte? LeafLabel = null;
+    public readonly int Frequency;
+    public readonly HuffmanNode Left;
+    public readonly HuffmanNode Right;
+    private readonly int _id;
+
+    public HuffmanNode(byte leafLabel, int frequency)
+    {
+        LeafLabel = leafLabel;
+        Frequency = frequency;
+        _id = ++Id;
+    }
+
+    public HuffmanNode(HuffmanNode left, HuffmanNode right)
+    {
+        Frequency = left.Frequency + right.Frequency;
+        Left = left;
+        Right = right;
+        _id = ++Id;
+    }
+
+    public override string ToString()
+    {
+        return $"[{_id}] => " + string.Join(" | ", Leafs.OrderBy(x => x));
+    }
+
+    public IEnumerable<byte> Leafs
+    {
+        get
+        {
+            if (LeafLabel != null)
+            {
+                return Enumerable.Empty<byte>().Append(LeafLabel.Value);
+            }
+
+            return Left.Leafs.Concat(Right.Leafs);
+        }
+    }
+    public override int GetHashCode()
+    {
+        return _id;
+    }
+}
